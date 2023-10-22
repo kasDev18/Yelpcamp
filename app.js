1f7589034d13a9563d8350ada1e909ff9d93bd0a
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2023-10-22 15:56:58 +0800

    fixes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campground');
 const reviewRoutes = require('./routes/reviews');
 
-const dbURL = process.env.DB_URL || "mongodb://127.0.0.1/yelp-camp";
+const dbURL = process.env.DB_URL;
 
 // const dbURL = "mongodb://127.0.0.1/yelp-camp";
 mongoose.connect(dbURL, {
